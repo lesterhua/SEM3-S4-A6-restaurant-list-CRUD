48ec4fb8df9f0177dcb96cdbeea32e44df826251
lester <servqua88@gmail.com>
2019-06-22 11:09:50 +0800

    Add handlebars , body-parser  and create main , index template

--- app.js
+++ app.js
@@ -1,26 +1,42 @@
+// require data
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Restaurant = require("./models/restaurant");
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
 
+const port = 2800;
+
+//connection MongoDB
 mongoose.connect("mongodb://127.0.0.1/restaurant", { useNewUrlParser: true });
 
+//setting express engine into handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//use body-parse on express
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Assign Mongoose to db
 const db = mongoose.connection;
 
+// db connecting confirm if error
 db.on("error", () => {
   console.log("mongodb error");
 });
 
+// db connecting confirm if success
 db.once("open", () => {
   console.log("mongodb connected!");
 });
 
-const port = 2800;
-
+//route setting for index page
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("index");
 });
 
+//starting and listen web server
 app.listen(port, () => {
   console.log(`Express app is running on : http://localhost:${port}`);
 });
